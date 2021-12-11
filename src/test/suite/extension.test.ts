26b775af0a326cc8856e7cf207ff4d3263c28031
Martin Kühl <martin.kuehl@posteo.net>
2021-12-11 22:49:24 +0100

    tests

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,98 @@
-import * as assert from 'assert';
+import { strict as assert } from 'assert';
+import * as crypto from 'crypto';
+import path = require('path');
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+suite('onDidOpenTextDocument', () => {
+	const disposables = new Set<vscode.Disposable>();
+	const opened = new Set<string>();
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	async function reset() {
+		await vscode.commands.executeCommand('workbench.action.closeAllEditors');
+		opened.clear();
+	}
+
+	suiteSetup(() => {
+		disposables.add(vscode.workspace.onDidOpenTextDocument((e) => {
+			opened.add(e.fileName);
+		}));
+		disposables.add(vscode.workspace.onDidCloseTextDocument((e) => {
+			opened.delete(e.fileName);
+		}));
+	});
+
+	suiteTeardown(() => {
+		disposables.forEach(d => d.dispose());
+	});
+
+	suite('for an existing file', () => {
+		async function create() {
+			const name = crypto.randomBytes(32).toString('hex');
+			const file = path.resolve(__dirname, '../../../tmp', name);
+			const uri = vscode.Uri.file(file);
+			await vscode.workspace.fs.writeFile(uri, Uint8Array.of());
+			return uri;
+		}
+
+		setup(reset);
+
+		test('command:vscode.open fires the event', async () => {
+			const uri = await create();
+			const file = uri.fsPath;
+			await vscode.commands.executeCommand('vscode.open', uri);
+			assert(opened.has(file), `'${file}' should be opened: {${[...opened].join(', ')}}`);
+			await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+			assert(!opened.has(file), `'${file}' should not be opened: {${[...opened].join(', ')}}`);
+			await vscode.commands.executeCommand('vscode.open', uri);
+			assert(opened.has(file), `'${file}' should be opened again: {${[...opened].join(', ')}}`);
+		});
+
+		test('openTextDocument fires the event', async () => {
+			const uri = await create();
+			const file = uri.fsPath;
+			let doc = await vscode.workspace.openTextDocument(uri);
+			await vscode.window.showTextDocument(doc);
+			assert(opened.has(file), `'${file}' should be opened: {${[...opened].join(', ')}}`);
+			await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+			assert(!opened.has(file), `'${file}' should not be opened: {${[...opened].join(', ')}}`);
+			doc = await vscode.workspace.openTextDocument(uri);
+			await vscode.window.showTextDocument(doc);
+			assert(opened.has(file), `'${file}' should be opened again: {${[...opened].join(', ')}}`);
+		});
+	});
+
+	suite('for a new file', () => {
+		function create() {
+			const name = crypto.randomBytes(32).toString('hex');
+			const file = path.resolve(__dirname, '../../../tmp', name);
+			return vscode.Uri.file(file).with({ scheme: 'untitled' });
+		}
+
+		setup(reset);
+
+		test('command:vscode.open fires the event', async () => {
+			const uri = create();
+			const file = uri.fsPath;
+			await vscode.commands.executeCommand('vscode.open', uri);
+			assert(opened.has(file), `'${file}' should be opened: {${[...opened].join(', ')}}`);
+			await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+			assert(!opened.has(file), `'${file}' should not be opened: {${[...opened].join(', ')}}`);
+			await vscode.commands.executeCommand('vscode.open', uri);
+			assert(opened.has(file), `'${file}' should be opened again: {${[...opened].join(', ')}}`);
+		});
+
+		test('openTextDocument fires the event', async () => {
+			const uri = create();
+			const file = uri.fsPath;
+			let doc = await vscode.workspace.openTextDocument(uri);
+			await vscode.window.showTextDocument(doc);
+			assert(opened.has(file), `'${file}' should be opened: {${[...opened].join(', ')}}`);
+			await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+			assert(!opened.has(file), `'${file}' should not be opened: {${[...opened].join(', ')}}`);
+			doc = await vscode.workspace.openTextDocument(uri);
+			await vscode.window.showTextDocument(doc);
+			assert(opened.has(file), `'${file}' should be opened again: {${[...opened].join(', ')}}`);
+		});
 	});
 });
